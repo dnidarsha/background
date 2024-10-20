3b98f9fc79f7e4d443a6876dfbdb2174f6ef8519
Nidarsha Dahal <dnidarsha@gmail.com>
2024-10-20 19:34:38 +0545

    my second push

--- Assets/movementenemy.cs
+++ Assets/movementenemy.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
 
-public class ManualEnemyMovement : MonoBehaviour
+public class EnemyMovement : MonoBehaviour
 {
-    public float speed = 5.0f;  // Speed of the enemy movement
+    public Transform player; // Reference to the player
+    public float speed = 2f; // Speed of enemy movement
+    public float followDistance = 5f; // Distance at which the enemy starts following
+
+    private Rigidbody2D rb; // Reference to the Rigidbody2D
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component
+    }
 
     void Update()
     {
-        // Get input from horizontal axis (left/right arrow keys or A/D keys)
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        // Get the distance between the player and the enemy
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
 
-        // Move the enemy based on input
-        Vector3 movement = new Vector3(moveHorizontal * speed * Time.deltaTime, 0, 0);
-        transform.Translate(movement);
+        // If within follow distance, move toward the player
+        if (distanceToPlayer < followDistance)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+
+            // Move the enemy horizontally toward the player
+            rb.velocity = new Vector2(direction.x * speed, rb.velocity.y);
+
+            // Flip the enemy to face the player
+            if (direction.x < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1); // Face left
+            }
+            else
+            {
+                transform.localScale = new Vector3(1, 1, 1); // Face right
+            }
+        }
+        else
+        {
+            // Stop the enemy's movement if out of range
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
     }
 }
